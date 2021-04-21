ee01e0704f0a8b9de5ad037512e6388bb68088f2
Lucas Alvarenga <lucasalvarenga020@gmail.com>
2021-04-21 14:42:40 -0300

    chore: add mutableLiveDatas

--- app/src/main/java/com/alvarengadev/marketplacelist/ui/fragments/add/AddViewModel.kt
+++ app/src/main/java/com/alvarengadev/marketplacelist/ui/fragments/add/AddViewModel.kt
@@ -1,7 +1,9 @@
 package com.alvarengadev.marketplacelist.ui.fragments.add
 
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
 class AddViewModel : ViewModel() {
-    // TODO: Implement the ViewModel
+    val valueItem = MutableLiveData<Double>()
+    val quantity = MutableLiveData<Int>()
 }
