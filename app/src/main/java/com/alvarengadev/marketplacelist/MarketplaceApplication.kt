5f3c6ad07c77554bb351233b4acc8f8865d146cf
Lucas Alvarenga <lucasalvarenga020@gmail.com>
2022-07-18 13:01:56 -0300

    add space

--- app/src/main/java/com/alvarengadev/marketplacelist/MarketplaceApplication.kt
+++
@@ -1,7 +0,0 @@
-package com.alvarengadev.marketplacelist
-
-import android.app.Application
-import dagger.hilt.android.HiltAndroidApp
-
-@HiltAndroidApp
-class MarketplaceApplication : Application()
